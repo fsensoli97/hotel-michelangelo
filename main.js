3f9b8a64f2585ad535323d40a3656d383bb7586b
fsensoli <fedesensoli1997@gmail.com>
2023-04-16 17:10:35 +0200

    deleted image swipe in gallery to allow zoom

--- main.js
+++ main.js
@@ -1,4 +1,96 @@
-function add_header_space() {
-  const height = document.getElementById('header_cont').offsetHeight;
-  document.body.style.marginTop = height + 'px';
-}
+const gallery = [
+  'images/front_terrace.jpg',
+  'images/room.jpg',
+  'images/terrace.jpg',
+  'images/front_building.jpg',
+  'images/view.jpg',
+  'images/restaurant.jpg',
+  'images/spumante.jpg',
+  'images/primo_pesce.jpg',
+  'images/terrace_2.jpg',
+  'images/building.jpg',
+  'images/cake.jpg',
+  'images/concerto.jpg',
+  'images/chef.jpeg',
+  'images/lasagna.jpeg',
+  'images/antipasto.jpeg',
+  'images/risotto.jpeg'
+]
+
+let current_img = 0;
+
+$('a.gallery_ref').on('click', function(){
+  
+  $('#popup_img').attr('src', $(this).attr('href'));
+  document.getElementById('popup').style.zIndex = 1000;
+  $('#popup').show();
+  $('#close_popup').show();
+  $('#next_img').show();
+  $('#prev_img').show();
+
+  for (let i = 0; i < gallery.length; i++) {
+    if (gallery[i] == $(this).attr('href')) {
+      current_img = i;
+      break;
+    }
+  }
+  return false;
+})
+
+$('#close_popup').on('click', function(){
+  $('#popup').hide();
+  $('#close_popup').hide();
+})
+
+$('#next_img').on('click', function(){
+  if (current_img == gallery.length-1) {
+    current_img = 0;
+  }
+  else {
+    current_img++;
+  }
+  $('#popup_img').attr('src', gallery[current_img]);
+})
+
+$('#prev_img').on('click', function(){
+  if (current_img == 0) {
+    current_img = gallery.length-1;
+  }
+  else {
+    current_img--;
+  }
+  $('#popup_img').attr('src', gallery[current_img]);
+})
+
+/*
+let touchstartX = 0
+let touchendX = 0
+
+document.getElementById('popup_img').addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX;
+})
+
+document.getElementById('popup_img').addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX;
+  if (touchendX < touchstartX) {
+    if (current_img == gallery.length-1) {
+    current_img = 0;
+    }
+    else {
+      current_img++;
+    }
+    $('#popup_img').attr('src', gallery[current_img]);
+  }
+  else {
+    if (current_img == 0) {
+    current_img = gallery.length-1;
+    }
+    else {
+      current_img--;
+    }
+    $('#popup_img').attr('src', gallery[current_img]);
+  }
+  touchstartX = 0;
+  touchendX = 0;
+})
+*/
